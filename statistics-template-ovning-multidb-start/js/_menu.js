329e5b7b1904573ee9564e1c0ea231caa4792f55
fadikasten468 <fadikasten468@gmail.com>
2026-05-01 20:26:25 +0200

    Update _menu.js

--- statistics-template-ovning-multidb-start/js/_menu.js
+++ statistics-template-ovning-multidb-start/js/_menu.js
@@ -1,6 +1,7 @@
 createMenu('Pengar & Politik', [
   { name: 'Översikt', script: 'start.js' },
   { name: 'Inkomst & Röster (%)', script: 'procent.js' },
+  { name: 'Jämför 2018 vs 2022', script: 'jamfor-2018-2022.js' },   // ← NY!
   { name: 'Län & Partier', script: 'lan.js' },
   { name: 'Källor', script: 'kallor.js' },
 ]);
